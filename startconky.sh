9e4b314629f3647c3688e008745cb47e490d2954
Makmiller <makmiller@gmail.com>
2013-08-14 02:08:28 -0600

    First commit

---
+++ startconky.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+conky -c /home/makmiller/config-files/general/conky/conky-main &
+# conky -c ~/.conkyrcs/conky-middle --alignment=top_middle &
+#conky -c ~/.conkyrcs/conky-messages --alignment=top_right &
