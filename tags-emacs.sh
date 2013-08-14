0e9cd0e1b88af45cc287851eb5faa40f5a1ab30d
Makmiller <makmiller@gmail.com>
2013-08-14 02:06:26 -0600

    First commit

---
+++ tags-emacs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# evolution:background folder
+/usr/bin/ctags \
+    -f /home/makmiller/Documents/mypapers/evolution/background/TAGS \
+    -e -R \
+    /home/makmiller/Documents/mypapers/evolution/background/
+    # /home/makmiller/Documents/mypapers/evolution/papers-in-progress/replicator-interactor-framework/replicator-interactor-framework.tex
+
+# logic folder
+/usr/bin/ctags \
+    -f /home/makmiller/Documents/mypapers/logic/TAGS \
+    -e -R \
+    /home/makmiller/Documents/mypapers/logic/
+
+    
+# htdX
+/usr/bin/ctags -f /home/makmiller/Documents/howtodo/TAGS -e \
+    /home/makmiller/Documents/howtodo/htdlinux/htdlinux.org \
+    /home/makmiller/Documents/howtodo/htdemacs/htdemacs.org \
+    /home/makmiller/Documents/howtodo/htdlatex/htdlatex.tex
+    
+# my emacs configs
+/usr/bin/ctags \
+    -f /home/makmiller/config-files/emacs-config/TAGS \
+    -e -R \
+    /home/makmiller/config-files/emacs-config/
+
+# my emacs agendas
+/usr/bin/ctags \
+    -f /home/makmiller/elisp/agenda/TAGS \
+    -e -R \
+    /home/makmiller/elisp/agenda/ \
+    /home/makmiller/elisp/cache/wasteclock.org
