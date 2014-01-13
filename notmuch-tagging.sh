2f2437846aa07e23b9465a636ec8df323e11aa00
Makmiller <makmiller@gmail.com>
2014-01-13 08:32:02 -0500

    Added tag for emails from TU account

--- notmuch-tagging.sh
+++ notmuch-tagging.sh
@@ -6,4 +6,5 @@ notmuch tag +sent folder:sent-mail
 notmuch tag +sent folder:gmail_sent
 notmuch tag +sent folder:sent
 notmuch tag +UNI folder:Uni
+notmuch tag +TU folder:TU
 notmuch search --output=files tag:delete | xargs -l rm
