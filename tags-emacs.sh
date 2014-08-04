aa501cd66d1169c6d6b490d0a355b49daa979da2
Makmiller <makmiller@gmail.com>
2014-08-03 22:19:55 -0400

    merge conflicts tag-emacs.sh
    Conflicts:
    	notmuch-tagging.sh
    	tags-emacs.sh

--- tags-emacs.sh
+++ tags-emacs.sh
@@ -5,18 +5,19 @@
 #     /home/makmiller/Documents/howtodo/htdlinux/htdlinux.org \
 #     /home/makmiller/Documents/howtodo/htdemacs/htdemacs.org \
 
-    
 # emacs config
 /usr/bin/ctags \
     -f /home/makmiller/config-files/general/emacs-config/TAGS \
     -e -R \
     /home/makmiller/config-files/general/emacs-config/
 
+
 # emacs agendas
 /usr/bin/ctags \
     -f /home/makmiller/elisp/agenda/TAGS \
     -e -R \
-    /home/makmiller/elisp/agenda/
+    /home/makmiller/elisp/agenda/ \
+    /home/makmiller/elisp/cache/wasteclock.org
 
 # social evolution project
 /usr/bin/ctags \
