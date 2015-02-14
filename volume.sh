77c70fc4dc514fdec260b97955fdcedc8f9f9b44
Makmiller <makmiller@gmail.com>
2015-02-14 13:48:16 -0500

    changed font xosd volume

--- volume.sh
+++ volume.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CHANNEL=Master
-#osd_cat --align=center --pos=bottom --color=darkgreen --font=lucidasanstypewriter-bold-24 --shadow=1 --barmode=percentage --percentage=0
+FONT="-*-*-bold-*-*-*-17-120-100-*-*-*-*-*"
 killall osd_cat
 volume=$(amixer get $CHANNEL | tail -n 1 | awk '{print $4}' | tr '[' ' ' | tr ']' ' ')
-echo "Volume: $volume" | osd_cat --align=center --pos=bottom --color=white --font=lucidasanstypewriter-bold-18 --shadow=1 --delay=2 #--barmode=percentage --percentage="$volume"
+echo "Volume: $volume" | osd_cat --align=center --pos=bottom --color=white  --font=$FONT --shadow=1 --delay=2 # --barmode=percentage --percentage="$volume"
 
 
 
