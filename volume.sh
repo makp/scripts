0d001ef17f4f9e5d1f250b63e6906c0b4bf2b993
Makmiller <makmiller@gmail.com>
2017-09-13 18:11:06 -0400

    fixed volume displayer

--- volume.sh
+++ volume.sh
@@ -3,7 +3,7 @@
 CHANNEL=Master
 FONT="-*-*-bold-*-*-*-17-120-100-*-*-*-*-*"
 killall osd_cat
-volume=$(amixer get $CHANNEL | tail -n 1 | awk '{print $4}' | tr '[' ' ' | tr ']' ' ')
+volume=$(amixer get $CHANNEL | tail -n 1 | awk '{print $5}' | tr '[' ' ' | tr ']' ' ')
 echo "Volume: $volume" | osd_cat --align=center --pos=bottom --color=white  --font=$FONT --shadow=1 --delay=2 # --barmode=percentage --percentage="$volume"
 
 
