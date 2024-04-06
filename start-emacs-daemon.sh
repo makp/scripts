8e4778461ec517a8a3559e54a3bf1a7ecae5a047
mak-leibniz <makmiller@gmail.com>
2024-04-06 12:20:55 -0400

    Add a todo item

--- start-emacs-daemon.sh
+++ start-emacs-daemon.sh
@@ -2,6 +2,7 @@
 # This script is used to start the Emacs daemon with systemd
 
 # Load some variables
+# TODO: Use the EnvironmentFile directive in the emacs.service instead
 source $HOME/.zshenv
 
 
