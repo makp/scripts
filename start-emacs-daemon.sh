91d2938368e4e89456b8a7175450d5d3bd0adf2f
mak-leibniz <makmiller@gmail.com>
2024-04-06 11:52:59 -0400

    Don't load ~./zlogin
    Sourcing the zlogin is a little hacky. That's because the systemd
    units are loaded independently of the shell initialization process.

--- start-emacs-daemon.sh
+++ start-emacs-daemon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script is used to start the Emacs daemon with systemd
 
-# Load some variables and start some processes
-source $HOME/.zlogin
+# Load some variables
 source $HOME/.zshenv
+
 
 # Start emacs daemon
 /usr/bin/emacs --daemon
