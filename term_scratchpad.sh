83be3299792d06d628dfcc025b396a68e83b4549
mak-turing <makmiller@gmail.com>
2024-04-23 14:19:26 -0400

    Don't attach terminal to tmux by default

--- term_scratchpad.sh
+++ term_scratchpad.sh
@@ -2,4 +2,4 @@
 alacritty --title 'term_scratchpad' \
 	--option window.dimensions.columns=88 \
 	--option window.dimensions.lines=14 \
-	--command zsh -c 'tmux attach -d'
+	# --command zsh -c 'tmux attach -d'
